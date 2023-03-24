016698841a0dae279f19beb6475109293bea1f14
Siling5304 <s111110525@student.nqu.edu.tw>
2023-03-24 10:50:30 +0800

    add truthtable.js

--- homework/05/truthtable.js
+++ homework/05/truthtable.js
@@ -1,9 +1,9 @@
 function truthTable(n){
     var t=Math.pow(2,n)
     for (var i = 0; i < t; i++) {
-        var x = i.toString(2).padStart(3, '0');
+        var x = i.toString(2).padStart(n, '0');
         console.log(x);
     }
 }
 
-truthTable(3)
+truthTable(2)
